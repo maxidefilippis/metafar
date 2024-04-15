26e1028a56a6c9487588be49fc4feb825a45195a
maxidefilippis <maxidefilippis@gmail.com>
2024-04-15 17:13:25 -0300

    added translations

--- src/services/i18n.ts
+++ src/services/i18n.ts
@@ -1,6 +1,6 @@
 import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import { components_es, home_es, layout_es } from '../translations';
+import { components_es, home_es, layout_es, api_es, detail_es } from '../translations';
 
 i18next.use(initReactI18next).init({
     lng: 'es',
@@ -10,6 +10,8 @@ i18next.use(initReactI18next).init({
             layout: layout_es,
             components: components_es,
             home: home_es,
+            api: api_es,
+            detail: detail_es,
         },
     },
 });
