8a5b557360368a3bed9b0a916d73e55735613463
maxidefilippis <maxidefilippis@gmail.com>
2024-04-15 17:13:16 -0300

    added translations

--- src/translations/index.ts
+++ src/translations/index.ts
@@ -2,5 +2,6 @@ import layout_es from './layout.json';
 import components_es from './components.json';
 import home_es from './home.json';
 import api_es from './api.json';
+import detail_es from './detail.json';
 
-export { home_es, layout_es, components_es, api_es };
+export { home_es, layout_es, components_es, api_es, detail_es };
