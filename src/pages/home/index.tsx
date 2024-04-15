4bf1430dea6ee2633a990a276946e87c5d2cee07
maxidefilippis <maxidefilippis@gmail.com>
2024-04-15 17:17:12 -0300

    updated homePage

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,25 +3,27 @@ import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
 import { Button } from '../../components/button';
 import { SearcIcon } from '../../components/icons/searchIcon';
-import { Input } from '../../components/input';
+import { InputText } from '../../components/input';
 import { Skeleton } from '../../components/skeleton';
 import { Table } from '../../components/table';
 import { Typography } from '../../components/typografhy';
 import { pageSize } from '../../constants/globals';
 import { Action } from '../../models/action';
-import { TextType } from '../../models/textType';
+import { TextType } from '../../constants/textType';
 import { getStockFromApi } from '../../redux/actions/getStock';
 import { useAppDispatch, useAppSelector } from '../../redux/hooks';
-import { setActionDetail, setFilteredActions, setSearch } from '../../redux/reducers/stockSlice';
+import { setActionDetail } from '../../redux/reducers/detailSlice';
+import { setFilteredActions, setSearch } from '../../redux/reducers/stockSlice';
 import { ActionsTable } from './components/actionsTable';
 import styles from './index.module.css';
 
-export const Home = () => {
+export const HomePage = () => {
     const { t } = useTranslation('home');
     const dispatch = useAppDispatch();
     const navigate = useNavigate();
 
-    const { actions, loading, search, filteredActions } = useAppSelector((store) => store.stock);
+    const { loading } = useAppSelector((store) => store.global);
+    const { actions, search, filteredActions } = useAppSelector((store) => store.stock);
     const [currentPage, setCurrentPage] = useState<number>(1);
     const [actionsList, setActionsList] = useState<Action[]>([]);
 
@@ -32,6 +34,12 @@ export const Home = () => {
         return Math.ceil(results.length / pageSize) || 1;
     }, [actions, search]);
 
+    const textPages = useMemo(() => t('TABLE.PAGE', { currentPage, totalPages }), [currentPage, totalPages]);
+    const textResults = useMemo(
+        () => t('TABLE.RESULTS', { pageSize: actionsList.length, count: search ? filteredActions.length : actions.length }),
+        [actionsList, search, filteredActions, actions],
+    );
+
     const handleBack = () => {
         setCurrentPage((page) => page - 1);
     };
@@ -40,14 +48,18 @@ export const Home = () => {
     };
     const seeActionDetail = (element: Action) => {
         dispatch(setActionDetail(element));
-        navigate(`/detail/${element.symbol}`);
+        navigate(`/detail/${element.symbol}/${element.exchange}`);
     };
     const handleFilterChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const search = e.target.value;
         dispatch(setSearch(search));
 
         if (search) {
-            const actionsFiltered = actions.filter((action) => action.name.toLowerCase().includes(search.toLowerCase()));
+            const actionsFiltered = actions.filter((action) => {
+                const searchMatchesName = action.name.toLowerCase().includes(search.toLowerCase());
+                const searchMatchesSymbol = action.symbol.toLocaleLowerCase().includes(search.toLowerCase());
+                return searchMatchesName || searchMatchesSymbol;
+            });
             dispatch(setFilteredActions(actionsFiltered));
         } else {
             dispatch(setFilteredActions([]));
@@ -74,22 +86,26 @@ export const Home = () => {
     return (
         <>
             <div className={styles.title}>
-            <Typography type={TextType.TITLE} text={t('HOME.TITLE')} />
+                <Typography type={TextType.TITLE} text={t('HOME.TITLE')} />
                 <div className={styles.searchBox}>
-                    <SearcIcon size={18} />
-                    <Input value={search} onChange={(e) => handleFilterChange(e)} />
+                    <SearcIcon size={20} />
+                    <InputText value={search} onChange={(e) => handleFilterChange(e)} />
                 </div>
             </div>
             <div className={styles.table}>
-                <Table heigth={470} children={<ActionsTable actions={actionsList} seeDetail={seeActionDetail}/>} />
+                <Table heigth={470} children={<ActionsTable actions={actionsList} seeDetail={seeActionDetail} />} />
             </div>
             <div className={styles.results}>
-                <Typography type={TextType.TEXT} text={t('TABLE.PAGE', { currentPage, totalPages })} />
+                <Typography type={TextType.TEXT} text={textPages} />
                 <div className={styles.buttons}>
-                    <Button onClick={handleBack} disabled={currentPage <= 1}>{t('BUTTONS.BACK')}</Button>
-                    <Button onClick={handleNext} disabled={currentPage >= totalPages}>{t('BUTTONS.NEXT')}</Button>
+                    <Button onClick={handleBack} disabled={currentPage <= 1}>
+                        {t('BUTTONS.BACK')}
+                    </Button>
+                    <Button onClick={handleNext} disabled={currentPage >= totalPages}>
+                        {t('BUTTONS.NEXT')}
+                    </Button>
                 </div>
-                <Typography type={TextType.TEXT} text={t('TABLE.RESULTS', { pageSize, count: actions.length })} />
+                <Typography type={TextType.TEXT} text={textResults} />
             </div>
         </>
     );
